32986ee160db20f3dcadbd90349c4be1caeb15c1
Wes Basinger <wbasinger@villagetechschools.org>
2016-08-21 20:33:10 -0500

    Added a yes/no radio group on the word page.

--- app.py
+++ app.py
@@ -2,14 +2,35 @@
 # -*- coding: utf-8 -*-
 
 from sys import argv
+from random import randint
 
 import bottle
 from bottle import default_app, request, route, response, get, template
 
 bottle.debug(True)
 
+
+
 @get('/')
 def index():
 	return template('index.tpl')
+
+@get('/word')
+def word():
+
+	word_list = [
+		{
+			"word": "born",
+			"is_anagram" : False 
+		},
+		{
+			"word" : "blue",
+			"is_anagram" : True
+		}
+	]
+	random_index = randint(0, len(word_list) - 1)
+	rand = word_list[random_index]
+	
+	return template('word.tpl', word = rand["word"])
 
 bottle.run(host='0.0.0.0', port=argv[1])
---
+++ views/word.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Anagram Game</title>
+	</head>
+	<body>
+		<h1>Anagram Game</h1>
+		<h2>Word: {{ word }}</h2>
+		<form action="/word" method="post">
+			<input type="radio" value="yes" name="is-anagram"/> Yes
+			<input type="radio" value="no" name="is-anagram" /> No
+			<input type="submit" value="Check" />
+		</form>
+	</body>
+</html>
